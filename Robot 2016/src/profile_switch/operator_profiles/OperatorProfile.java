70358f704544a71ddad5d2651410c6a4ceba5fbd
Max <mxmeinhold@gmail.com>
2016-01-28 18:29:08 -0500

    Added intake switching

--- Robot 2016/src/profile_switch/operator_profiles/OperatorProfile.java
+++ Robot 2016/src/profile_switch/operator_profiles/OperatorProfile.java
@@ -7,41 +7,45 @@ public class OperatorProfile {
 	//Act Methods
 	public void shoot()
 	{
-		//Normal shoot
-		if(Variables.joyOp.getRawButton(Variables.X))
+		//Normal shoot (buttons x and b)
+		if(Variables.joyOp.getRawButton(Variables.X) || Variables.joyOp.getRawButton(Variables.B))
 				System.out.print("");//code to shoot
-		//Reverse shoot
-		if(Variables.joyOp.getRawButton(Variables.B))
-				System.out.print("");//code to reverse shoot
 	}
 	
 	public void arm()
 	{
-		//Up arm
+		//Up arm (button y)
 		if(Variables.joyOp.getRawButton(Variables.Y))
 				System.out.print("");//code to arm
-		//Down arm
+		//Down arm (button a)
 		if(Variables.joyOp.getRawButton(Variables.A))
 				System.out.print("");//code to reverse arm
 	}
 	
 	public void intake()
 	{
-		//intake
+		//intake (button lb)
 		if(Variables.joyOp.getRawButton(Variables.LEFT_BUMPER))
 				System.out.print("");//code to intake
-		//outtake
+		//outtake (button rb)
 		if(Variables.joyOp.getRawButton(Variables.RIGHT_BUMPER))
 				System.out.print("");//code to reverse intake
 	}
 	
 	public void intakeArm()
 	{
-		//Forward intakeArm
+		//Forward intakeArm (button lt)
 		if(Variables.joyOp.getRawButton(Variables.LEFT_TRIGGER))
 				System.out.print("");//code to intakeArm
-		//Reverse intakeArm
+		//Reverse intakeArm (button rt)
 		if(Variables.joyOp.getRawButton(Variables.RIGHT_TRIGGER))
 				System.out.print("");//code to reverse intakeArm
+	}
+	
+	public void intakeSwitcher()
+	{
+		//switches to forward or backwards intake based on which button on the dpad is pressed (up or down)
+		if((Variables.forwardIntake && Variables.joyOp.getPOV() == 4) || (!Variables.forwardIntake && Variables.joyOp.getPOV() == 0))
+			Variables.forwardIntake = !Variables.forwardIntake;
 	}
 }
