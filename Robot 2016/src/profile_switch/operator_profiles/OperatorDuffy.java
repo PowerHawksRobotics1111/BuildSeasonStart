5e6b251685600c2a3d7973c27c08454c14c0a666
Max <mxmeinhold@gmail.com>
2016-02-01 19:20:33 -0500

    Override button references in Operator profiles.

--- Robot 2016/src/profile_switch/operator_profiles/OperatorDuffy.java
+++ Robot 2016/src/profile_switch/operator_profiles/OperatorDuffy.java
@@ -5,4 +5,16 @@ import Variables.Motors;
 
 public class OperatorDuffy extends OperatorProfile {
 	
+	public OperatorDuffy()
+	{
+		super.shootButton = Joysticks.LEFT_TRIGGER;
+		super.intakeButton = Joysticks.RIGHT_BUMPER;
+		super.outtakeButton = Joysticks.LEFT_BUMPER;
+		//Move arm up same as super
+		//move arm down same as super
+		super.tapeArmExtend = Joysticks.Y;
+		super.tapeArmRetract = Joysticks.A;
+		//TODO Override the control for the tape arm rotate to operate on stick.
+		//Override modifier same as super.
+	}
 }
