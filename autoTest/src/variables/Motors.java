bbb2e40c7cc3d8ff6437edc98ba7c5dbfdd1433f
Duffy <legohead259@gmail.com>
2016-02-06 18:29:36 -0500

    "Motors" Class Updated
    "Motors" class has been finished for what we have now. Appropriate
    variables and motors have been mapped in and documented. Any auxiliary
    motors have to determined and mapped.

--- autoTest/src/variables/Motors.java
+++ autoTest/src/variables/Motors.java
@@ -3,8 +3,71 @@ package variables;
 import edu.wpi.first.wpilibj.CANTalon;
 
 public class Motors {
-	public final double QUARTER_POWER = .25, REVERSE_QUARTER_POWER = -.25, NO_POWER = 0;
 	
-	public CANTalon testMotorFrontLeft = new  CANTalon(47), testMotorFrontRight = new CANTalon(46), testMotorBackLeft = new CANTalon(57),
-			 		testMotorBackRight = new CANTalon(51);
+	
+	//*****START MOTOR POWER VARAIBLES*****
+	
+	
+	/**
+	 * Sets motor to full power in respective direction
+	 */
+	public final int FULL_POWER = 1, REVERSE_FULL_POWER = -1;
+	
+	/**
+	 * Sets motor speed to three quarters power in respective direction
+	 */
+	public final double THREE_Q_POWER = .75, REVERSE_THREE_Q_POWER = -.75;
+	
+	/**
+	 * Sets motor speed to half power in respective direction
+	 */
+	public final double HALF_POWER = .5, REVERSE_HALF_POWER = -.5;
+	
+	/**
+	 * Sets motor speed to quarter power in respective direction
+	 */
+	public final double QUARTER_POWER = .25, REVERSE_QUARTER_POWER = -.25; 
+	
+	/**
+	 * Sets motor to no power
+	 */
+	public final int NO_POWER = 0;
+	
+	
+	//*****END MOTOR POWER VARIABLES*****
+	
+	
+	//*****START DRIVE MOTORS*****
+	
+	/**
+	 * Front left drive motor
+	 */
+	public CANTalon driveMotorFrontLeft = new  CANTalon(47); 
+	
+	/**
+	 * Front right drive motor
+	 */
+	public CANTalon driveMotorFrontRight = new CANTalon(46);
+	
+	/**
+	 * Back left drive motor
+	 */
+	public CANTalon driveMotorBackLeft = new CANTalon(57);
+	
+	/**
+	 * Back right drive motor
+	 */
+	public CANTalon driveMotorBackRight = new CANTalon(51);
+	
+	
+	//*****END DRIVE MOTORS*****
+	
+	
+	//*****START AUXILLARY MOTORS*****
+	
+	
+	//TODO Determine and map in auxillary motors
+	
+	
+	//*****END AUXILLARY MOTORS*****
 }
