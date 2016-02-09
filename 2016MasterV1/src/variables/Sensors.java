559dc6a08503116ab146e090afb0387e12b4b1f6
Max <mxmeinhold@gmail.com>
2016-02-09 18:55:40 -0500

    Better Define name of limit switch reference variable.

--- 2016MasterV1/src/variables/Sensors.java
+++ 2016MasterV1/src/variables/Sensors.java
@@ -4,6 +4,6 @@ import edu.wpi.first.wpilibj.DigitalInput;
 
 public class Sensors {
 
-	public static final DigitalInput limitSwitch = new DigitalInput(
+	public static final DigitalInput intakeLimitSwitch = new DigitalInput(
 			Motors.LS_PORT);
 }
