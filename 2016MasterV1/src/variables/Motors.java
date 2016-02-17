9e73a21f199aaee184ce74c300f233a7316204ea
Max <mxmeinhold@gmail.com>
2016-02-17 16:52:24 -0500

    Condensed motor init.
    Begun fixing Motor class for clarity.

--- 2016MasterV1/src/variables/Motors.java
+++ 2016MasterV1/src/variables/Motors.java
@@ -36,14 +36,8 @@ public class Motors {
 	 */
 	final static int MDFR_PORT = 1, MDFL_PORT = 1, MDBR_PORT = 1, MDBL_PORT = 1;
 
-	/**
-	 * Forward power levels
-	 */
 	public final static double FULL_POWER = 1.0, THREE_QUARTERS_POWER = .75, HALF_POWER = .5, QUARTER_POWER = .25, NO_POWER = 0.0;
 
-	/**
-	 * Reverse power levels
-	 */
 	public final static double REVERSE_FULL_POWER = -1.0, REVERSE_THREE_QUARTERS_POWER = -.75, REVERSE_HALF_POWER = -.5, REVERSE_QUARTER_POWER = -.25;
 
 	public static final double INTAKE_POWER = 0.0, OUTER_INTAKE_POWER = 0.0, ARM_POWER = 0.0, SHOOTER_POWER = 0.0, TAPE_EXT_POWER = 0.0, TAPE_ROT_POWER = 0.0;
@@ -51,50 +45,22 @@ public class Motors {
 	
 	
 	/**
-	 * Method that initializes the motors on the robot
+	 * Motor initialization
 	 */
 	public static void motorInit()
-	{
-		driveMotorInit();
-		armMotorInit();
-		intakeMotorInit();
-		shooterMotorInit();
-	}
-
-	/**
-	 * Method that initializes the drive motors
-	 */
-	private static void driveMotorInit()
 	{
 		motorDriveFrontRight = new CANTalon(MDFR_PORT);
 		motorDriveFrontLeft = new CANTalon(MDFL_PORT);
 		motorDriveBackRight = new CANTalon(MDBR_PORT);
 		motorDriveBackLeft = new CANTalon(MDBL_PORT);
-	}
-
-	/**
-	 * Method that initializes the arm manipulation motors
-	 */
-	private static void armMotorInit()
-	{
+		
 		motorArm = new CANTalon(MA_PORT);
+		
 		motorTapeArmExt = new CANTalon(MTAE_PORT);
 		motorTapeArmRot = new CANTalon(MTAR_PORT);
-	}
-
-	/**
-	 * Method that initializes the shooter motor
-	 */
-	private static void shooterMotorInit()
-	{
+		
 		motorShooter = new CANTalon(MS_PORT);
-	}
-
-	/**
-	 * Method that initializes the arm/chassis intake motors
-	 */
-	private static void intakeMotorInit()
-	{
+		
 		motorIntake = new CANTalon(MI_PORT);
 		motorOuterIntake = new CANTalon(MOI_PORT);
 	}
