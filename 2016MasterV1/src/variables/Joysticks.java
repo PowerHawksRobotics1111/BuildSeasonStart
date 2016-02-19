2f051252687f1950053b4c2f1445cf19928cd877
Shannon <lizomalley16@yahoo.com>
2016-02-18 19:56:39 -0500

    Condenced Joysticks class

--- 2016MasterV1/src/variables/Joysticks.java
+++ 2016MasterV1/src/variables/Joysticks.java
@@ -4,34 +4,22 @@ import edu.wpi.first.wpilibj.Joystick;
 
 public class Joysticks {
 
-	/**
-	 * Joystick port numbers
-	 */
+	//Joystick port numbers
 	private final static int DRIVER_PORT = 0, OPERATOR_PORT = 1;
 
-	/**
-	 * Button number references.
-	 */
+	//Button numbers
 	final static int X = 1, A = 2, B = 3, Y = 4, LEFT_BUMPER = 5,
 			RIGHT_BUMPER = 6, LEFT_TRIGGER = 7, RIGHT_TRIGGER = 8, BACK = 9, START = 10;
 
-	/**
-	 * Named references for the POV directions.
-	 */
 	public final static int D_PAD_UP = 0, D_PAD_FORWARD_RIGHT = 45,
 			D_PAD_RIGHT = 90, D_PAD_BACKWARD_RIGHT = 135, D_PAD_DOWN = 180,
 			D_PAD_BACKWARD_LEFT = 225, D_PAD_LEFT = 270, D_PAD_FORWARD_LEFT = 315,
 			D_PAD_OFF = -1;
 
-	/**
-	 * References for joysticks
-	 */
 	public final static Joystick joyDrive = new Joystick(DRIVER_PORT),
 			joyOp = new Joystick(OPERATOR_PORT);
 
-	/**
-	 * Named references based on the operator profile.
-	 */
+	/** Named references based on main operator preferences */
 	public static class Buttons {
 
 		public static final int shootButton = X;
