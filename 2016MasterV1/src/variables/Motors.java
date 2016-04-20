209dd50a6f1d79cbd48f469c51aac8443a997405
Max <mxmeinhold@gmail.com>
2016-04-20 07:32:31 -0400

    Shooter power adjustment Dashboard control for demo. Default is .3625 (Half of competition value)

--- 2016MasterV1/src/variables/Motors.java
+++ 2016MasterV1/src/variables/Motors.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.CANTalon.TalonControlMode;
 import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Motors {
 
@@ -29,8 +30,9 @@ public class Motors {
 	//Motor power variables for forward, reverse, intake, outer intake, arm, shooter, and tape arm
 	public final static double FULL_POWER = 1.0, THREE_QUARTERS_POWER = .75, HALF_POWER = .5, QUARTER_POWER = .25, NO_POWER = 0.0;
 	public final static double REVERSE_FULL_POWER = -1.0, REVERSE_THREE_QUARTERS_POWER = -.75, REVERSE_HALF_POWER = -.5, REVERSE_QUARTER_POWER = -.25;
-	public static final double INNER_INTAKE_POWER = .68, OUTER_INTAKE_POWER = .60/*was  .50*/, OUTER_OUTAKE_POWER = -.80, ARM_POWER = -.58, SHOOTER_POWER = .725, TAPE_ARM_POWER = .75, INNER_OUTAKE_POWER = -1.0, ARM_DOWN_POWER = .50;
+	public static final double INNER_INTAKE_POWER = .68, OUTER_INTAKE_POWER = .60/*was  .50*/, OUTER_OUTAKE_POWER = -.80, ARM_POWER = -.58, /*SHOOTER_POWER = .725,*/ TAPE_ARM_POWER = .75, INNER_OUTAKE_POWER = -1.0, ARM_DOWN_POWER = .50;
 
+	public static double SHOOTER_POWER = .3625;
 	//public static final double BRAKE_ANGLE = 120; //TODO find accurate breaking angle
 	
 	public static final double BACK_WHEEL_DRIVE_RATIO = 7.66/8.0; //TODO this is to adjust for the pneumatic wheels if we need to!!!
@@ -38,6 +40,17 @@ public class Motors {
 	public static final double flashlightVoltage = 9.0;
 	//private static final int ARM_TICKS_PER_REV = 18;
 
+	
+	public static void shooterSpeedDashboardInit()
+	{
+		SmartDashboard.putString("Shooter Adjustment Instructions", "Shooter speed must be positive double less then 1.0." + "/n" + "Competition shooter speed is .725." + "/n" + "Default speed for demo is .3625");
+		SmartDashboard.putNumber("Shooter Speed Setting", SHOOTER_POWER);
+	}
+	
+	public static void shooterSpeedDashboardUpdate()
+	{
+		SHOOTER_POWER = SmartDashboard.getNumber("Shooter Speed Setting");
+	}
 
 	/** Motor initialization*/
 	public static void motorInit()
